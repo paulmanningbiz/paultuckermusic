c351522a76b916cc7e61203f120dc2f62fc62176
g@paulmanning.biz <g@paulmanning.biz>
2013-09-10 11:50:34 +0100

    Articles

---
+++ Articles/Biography.php
@@ -0,0 +1,17 @@
+<article>
+
+	<h2>Biography</h2>
+
+	Paul Tucker is a composer/pianist currently teaching, performing and writing in West London.
+	He studied at the Royal Welsh College of Music and Drama and the London College of music and 
+	Media. 
+	
+	He graduated in 2001 with a Masters degree in composition for Film and Television, 
+	His Acclaimed compositions include works for solo piano, chamber ensembles, Choral music, 
+	theatre music and full orchestra. 
+	
+	He has won many piano competitions, and also the ABAC Premio 
+	Franz Liszt International Competition for composers, held in Grottamare, Italy.
+	</p>
+	
+</article>
